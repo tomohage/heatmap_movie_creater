8b25253f251b998d2816e29f0052d910987e14c0
有吉智貴 <tomoki@ariyoshisatoshiki-no-MacBook-Pro.local>
2017-04-06 04:50:01 +0900

    first commit

---
+++ heatmap_creater.py
@@ -0,0 +1,240 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import heatmap
+import csv
+import os
+import glob
+import sys
+import shutil
+
+from PIL import Image, ImageDraw
+
+
+
+_MONITOR_SIZE_ROW = 2
+_POSITION_DATA_ROW = 5
+
+_TRIGGER_OFF_STATUS = 0
+_TRIGGER_ON_STATUS = 1
+
+
+# 指定したfpsで動画を作成できない場合はException
+def validate_fps(csv_file_path, fps):
+    unit_time = 1.0 / fps * 1000
+
+    csv_file = open(csv_file_path, 'rb')
+    csv_reader = csv.reader(csv_file)
+
+    previous_time = None
+    time = None
+    min_unit_time = None
+    row_num = 0
+    for row in csv_reader:
+        if row_num > _POSITION_DATA_ROW:
+            if time is None:
+                time = float(row[0])
+                continue
+            previous_time = time
+            time = float(row[0])
+            if min_unit_time is None or min_unit_time > time - previous_time:
+                min_unit_time = time - previous_time
+                if unit_time < min_unit_time:
+                    csv_file.close()
+                    raise Exception('[Invalid] This fps is too large.')
+        row_num += 1
+    csv_file.close()
+
+
+def create_heatmap_movie(csv_file_name, fps, window_size):
+    current_dir_path = os.getcwd()
+
+    validate_fps(current_dir_path + '/' + csv_file_name, fps)
+
+    csv_file = open(current_dir_path + '/' + csv_file_name, 'rb')
+    csv_reader = csv.reader(csv_file)
+    os.makedirs(current_dir_path + "/images")
+
+    # fpsから時系列の時間間隔単位を取得
+    unit_time = 1.0 / fps * 1000
+
+    print("fps:" + str(fps) + " unit time:" + str(unit_time) + "[ms]")
+    print("create heatmap images")
+
+    file_num = 0
+    row_num = 1
+    monitor_size = None
+    now_time = 0
+    pts = []
+
+    # ヒートマップの連番画像を作成
+    for row in csv_reader:
+        if row_num == _MONITOR_SIZE_ROW:
+            monitor_size = [int(float(row[3])), int(float(row[1]))]
+            print monitor_size
+        elif row_num > _POSITION_DATA_ROW:
+            time = float(row[0])
+            x = int(float(row[1]))
+            y = monitor_size[1] - int(float(row[2]))
+            if len(pts) == window_size:
+                del (pts[0])
+            pts.append([x, y])
+            if now_time > time:
+                continue
+            now_time = int(now_time + unit_time)
+            hm = heatmap.Heatmap()
+            img = hm.heatmap(
+                points = pts,
+                size = monitor_size,
+                dotsize = 100,
+                area = ((0, 0), (monitor_size[1], monitor_size[0])),
+                scheme = 'classic',
+                opacity = 150
+            )
+            print(str(now_time) + "[ms] complete")
+            img.save(current_dir_path + "/images/PNG%05d.png" % file_num)
+            file_num += 1
+        row_num += 1
+    csv_file.close()
+
+    files = glob.glob(current_dir_path + '/images/*.png')
+
+    i = 0
+    for f in files:
+        os.rename(f, current_dir_path + '/images/%05d.png' % i)
+        i += 1
+
+    cmd = 'ffmpeg -r ' + str(fps) + ' -i images/%05d.png -pix_fmt yuv420p -vcodec libx264 heatmap_movie.mp4'
+    os.system(cmd)
+    shutil.rmtree('images/')
+
+    print("finish creating movie file.")
+
+def create_heatmap_images(csv_file_name, fps, window_size):
+    current_dir_path = os.getcwd()
+
+    validate_fps(current_dir_path + '/' + csv_file_name, fps)
+
+    os.makedirs(current_dir_path + "/images")
+
+    # fpsから時系列の時間間隔単位を取得
+    unit_time = 1.0 / fps * 1000
+
+    print("fps:" + str(fps) + " unit time:" + str(unit_time) + "[ms]")
+    print("create heatmap images")
+
+    file_num = 0
+    row_num = 1
+    monitor_size = None
+    now_time = 0
+
+    # 視点座標の円形図を出力するためのlist
+    pts = []
+    # ヒートマップを出力するためのlist
+    heatmap_pts = []
+
+    count = 0
+    time = 0
+    csv_file = open(current_dir_path + '/' + csv_file_name, 'rb')
+    csv_reader = csv.reader(csv_file)
+    for row in csv_reader:
+        if row_num > _POSITION_DATA_ROW:
+            count += 1
+            time = float(row[0])
+        row_num += 1
+    span = float(time) / float(count)
+    csv_file.close()
+
+    # ヒートマップの連番画像を作成
+    csv_file = open(current_dir_path + '/' + csv_file_name, 'rb')
+    csv_reader = csv.reader(csv_file)
+    row_num = 1
+    status = _TRIGGER_OFF_STATUS
+    for row in csv_reader:
+        if row_num == _MONITOR_SIZE_ROW:
+            monitor_size = [int(float(row[1])), int(float(row[3]))]
+        elif row_num > _POSITION_DATA_ROW:
+            file_num += 1
+            if len(row) < 4:
+                status = _TRIGGER_OFF_STATUS
+                continue
+
+            time = float(row[0])
+            if now_time > time:
+                continue
+            if status == _TRIGGER_OFF_STATUS and str.strip(row[3]) == 'T':
+                now_time = int(time)
+                status = _TRIGGER_ON_STATUS
+            else:
+                now_time = int(now_time + unit_time)
+
+            if len(pts) == window_size:
+                del (pts[0])
+                del (heatmap_pts[0])
+
+            heatmap_pts.append([int(float(row[1])), monitor_size[1] - int(float(row[2]))])
+            pts.append([int(float(row[1])), int(float(row[2]))])
+
+            hm = heatmap.Heatmap()
+            img = hm.heatmap(
+                points = heatmap_pts,
+                size = (monitor_size[0], monitor_size[1]),
+                dotsize = 200,
+                area = ((0, 0), (monitor_size[0], monitor_size[1])),
+                scheme = 'classic',
+                opacity = 200
+            )
+
+            dr = ImageDraw.Draw(img, 'RGBA')
+
+            # windowsサイズにある視点を追従する形で出力する
+            # 現在の視点座標を一番大きくする
+            count = 0
+            end = len(pts) - 1
+            for pt in pts:
+                if count == end:
+                    ellipse_size = 60
+                else:
+                    ellipse_size = 20
+                dr.ellipse(
+                    (
+                        int(float(pt[0])) - ellipse_size / 2,
+                        int(float(pt[1])) - ellipse_size / 2,
+                        int(float(pt[0])) + ellipse_size / 2,
+                        int(float(pt[1])) + ellipse_size / 2
+                    ),
+                    outline = (255, 255, 255, 200),
+                    fill = (255, 255, 255, 200)
+                )
+                if count != 0:
+                    dr.line(
+                        (
+                            int(float(pts[count - 1][0])),
+                            int(float(pts[count - 1][1])),
+                            int(float(pts[count][0])),
+                            int(float(pts[count][1]))
+                        ),
+                        fill = (255, 255, 255, 200),
+                        width = 5
+                    )
+                count += 1
+
+            del dr
+
+            print(str(now_time) + "[ms] complete")
+            # 時系列の時間をファイル名にして画像ファイルを出力
+            # ms単位で10桁でファイル名を設定
+            img.save(current_dir_path + "/images/%010d.png" % int(now_time))
+        row_num += 1
+    csv_file.close()
+
+    print("finish creating image files and dir.")
+
+    return span
+
+
+if __name__ == "__main__":
+    csv_file = 'recording_data.csv'
+    fps = 5
+    window_size = 10
+    #create_heatmap_movie(csv_file, fps, window_size)
+    create_heatmap_images(csv_file, fps, window_size)
---
+++ main.py
@@ -0,0 +1,144 @@
+#coding=utf-8
+import cv2
+import glob
+import os
+import sys
+from PIL import Image
+import numpy
+import shutil
+
+import heatmap_creater
+
+# PILを使って画像を合成
+def overlayOnPart(src_image, overlay_image, posX, posY):
+
+    # オーバレイ画像のサイズを取得
+    ol_height, ol_width = overlay_image.shape[:2]
+
+    # OpenCVの画像データをPILに変換
+    #　BGRAからRGBAへ変換
+    src_image_RGBA = cv2.cvtColor(src_image, cv2.COLOR_BGR2RGB)
+    overlay_image_RGBA = cv2.cvtColor(overlay_image, cv2.COLOR_BGRA2RGBA)
+
+    #　PILに変換
+    src_image_PIL=Image.fromarray(src_image_RGBA)
+    overlay_image_PIL=Image.fromarray(overlay_image_RGBA)
+
+    # 合成のため、RGBAモードに変更
+    src_image_PIL = src_image_PIL.convert('RGBA')
+    overlay_image_PIL = overlay_image_PIL.convert('RGBA')
+
+    # 同じ大きさの透過キャンパスを用意
+    tmp = Image.new('RGBA', src_image_PIL.size, (255, 255, 255, 0))
+    # 用意したキャンパスに上書き
+    tmp.paste(overlay_image_PIL, (posX, posY), overlay_image_PIL)
+    # オリジナルとキャンパスを合成して保存
+    result = Image.alpha_composite(src_image_PIL, tmp)
+
+    # COLOR_RGBA2BGRA から COLOR_RGBA2BGRに変更。アルファチャンネルを含んでいるとうまく動画に出力されない。
+    return  cv2.cvtColor(numpy.asarray(result), cv2.COLOR_RGBA2BGR)
+
+def resize_image(image, height, width):
+
+    # 元々のサイズを取得
+    org_height, org_width = image.shape[:2]
+    # 大きい方のサイズに合わせて縮小
+    height_ratio = float(height) / org_height
+    width_ratio = float(width) / org_width
+    resized = cv2.resize(image, (int(width), int(height)))
+
+    return resized
+
+if __name__ == "__main__":
+    args = sys.argv
+    print args
+    if len(args) != 6:
+        print u"引数を以下のように定義してください"
+        print u"1: アイトラッカーの録画CSVファイルのパス"
+        print u"2: 録画動画のファイルパス"
+        print u"3: ヒートマップ開始の時刻[ms]"
+        print u"4: 出力ファイルの名前"
+        print u"5: 音無し版のファイルも残しておくフラグ(1を指定)"
+        exit()
+    recording_csv_path = str(args[1])
+    recording_movie_path = str(args[2])
+    heatmap_start_ms = int(args[3])
+    output_file_name = str(args[4])
+    is_saved_movie_no_sound = str(args[5])
+
+    # ヒートマップ画像作成
+    heatmap_fps = 5
+    window_size = 10
+    print u"##### START CREATING HEATMAP IMAGE #####"
+    heatmap_creater.create_heatmap_images(recording_csv_path, heatmap_fps, window_size)
+    print u"##### FINISH CREATING HEATMAP IMAGE #####"
+
+    current_dir_path = os.getcwd()
+    files = glob.glob(current_dir_path + '/images/*.png')
+    start_file, ext = os.path.splitext(os.path.basename(files[0]))
+    end_file, ext = os.path.splitext(os.path.basename(files[-1]))
+    span = (float(end_file) - float(start_file)) / len(files)
+
+    # 読み込む
+    cmd = 'ffmpeg -i ' + recording_movie_path + ' -r 30 -ss ' + str(heatmap_start_ms / 1000.0) + ' temp_cut_movie.m4v'
+    os.system(cmd)
+    cmd = 'ffmpeg -i temp_cut_movie.m4v -vn temp_cut_aud.mp4'
+    os.system(cmd)
+
+    movie = cv2.VideoCapture('temp_cut_movie.m4v')
+    fps = int(round(movie.get(cv2.cv.CV_CAP_PROP_FPS), 0))
+    width = movie.get(cv2.cv.CV_CAP_PROP_FRAME_WIDTH)
+    height = movie.get(cv2.cv.CV_CAP_PROP_FRAME_HEIGHT)
+    total_frame = movie.get(cv2.cv.CV_CAP_PROP_FRAME_COUNT)
+
+    # コーデックの指定
+    fourcc = cv2.cv.CV_FOURCC('m', 'p', '4', 'v')
+
+    # 保存ファイルとフレームレートとサイズの指定
+    out = cv2.VideoWriter('no_sound_'+ output_file_name, int(fourcc), fps, (int(width), int(height)))
+
+    if movie.isOpened() == True:
+        ret, frame = movie.read()
+    else:
+        ret = False
+
+    print u"##### START CREATING HEATMAP MOVIE #####"
+    print u"0[%%] (0/%d)" % total_frame
+    percent_count = 1
+    frame_count = 0
+    while ret:
+        frame_count += 1
+        now_ms = float(movie.get(cv2.cv.CV_CAP_PROP_POS_MSEC))
+        if frame_count == int(percent_count * 0.1 * total_frame):
+            print u"%d[%%] (%d/%d)" % ((percent_count * 10), frame_count, total_frame)
+            percent_count += 1
+
+        if int(now_ms / span) < len(files):
+            heatmap_img_path = files[int(now_ms / span)]
+            heatmap_img = cv2.imread(heatmap_img_path, cv2.IMREAD_UNCHANGED)
+            heatmap_img = resize_image(heatmap_img, height, width)
+            frame = overlayOnPart(frame, heatmap_img, 0, 0)
+            rgba = cv2.cvtColor(frame, cv2.COLOR_RGB2RGBA)
+            # 書き出し
+            out.write(frame)
+            cv2.imshow('frame', frame)
+
+        if frame_count == total_frame:
+            break
+        ret, frame = movie.read()
+    movie.release()
+    out.release()
+    cv2.destroyAllWindows()
+    print u"##### FINISH CREATING HEATMAP MOVIE #####"
+
+    # 音声付加
+    cmd = 'ffmpeg -i no_sound_' + output_file_name + ' -i temp_cut_aud.mp4 -vsync -1 ' + output_file_name
+    os.system(cmd)
+
+    # いらないファイルの削除
+    os.remove('temp_cut_movie.m4v')
+    os.remove('temp_cut_aud.mp4')
+    if is_saved_movie_no_sound != '1':
+        os.remove('no_sound_' + output_file_name)
+
+    shutil.rmtree('images/')
